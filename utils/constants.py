84e10dbecb3178a6285802bd3c02a6de06f7eb7e
Sharmaine <sharmainecheongg@gmail.com>
2025-07-01 10:32:12 +0800

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,20 +1,23 @@
 REGIONS = {
-  'TH': 'Thailand',
-  'SG': 'Singapore',
-  'VN': 'Vietnam',
-  'KH': 'Cambodia', 
+    'TH': 'Thailand',
+    'SG': 'Singapore',
+    'VN': 'Vietnam',
+    'KH': 'Cambodia',
+    'HK': 'Hong Kong',
 }
 
 IDS = {
-  'SG': 1,
-  'VN': 2,
-  'KH': 3,
-  'TH': 8,
+    'SG': 1,
+    'VN': 2,
+    'KH': 3,
+    'TH': 8,
+    'HK': 7,
 }
 
 TIMEZONES = {
-  'TH': 7,
-  'SG': 8,
-  'VN': 7,
-  'KH': 7,
+    'TH': 7,
+    'SG': 8,
+    'VN': 7,
+    'KH': 7,
+    'HK': 8, 
 }
